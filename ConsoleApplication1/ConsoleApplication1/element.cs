d7939711330a99b173e7952e6c6860785b7a00fd
powbjn <powbjn@gmail.com>
2016-09-26 09:27:42 +0700

    [pldp;,ol-p[kop

---
+++ ConsoleApplication1/ConsoleApplication1/element.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class element<T>
+    {
+        private T data;
+
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        element<T> next;
+        internal element<T> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+    }
+}
